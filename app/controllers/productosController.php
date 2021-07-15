d9b9c1572e243f974b499cb3f0432cca5ce72bd3
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2021-07-14 23:17:11 -0300

    no funciona la bse

---
+++ app/controllers/productosController.php
@@ -0,0 +1,72 @@
+<?php
+
+class productosController{
+
+        public function RetornarProductos($request, $response, $args){
+            $listaDeParametros = $request->getParsedBody();
+
+            $jsonproductos = Producto::obtenerProducto();
+            $response->getBody()->Write(json_encode($jsonpeliculas));
+            return $response ->withHeader('Content-Type', 'application/json');
+
+        }
+        public function Alta($request, $response, $args){
+            $listaDeParametros = $request->getParsedBody();
+        
+
+                $productos = new Producto();
+                $productos->id_producto = $listaDeParametros['id_producto'];
+                $productos->nombre = $listaDeParametros['nombre'];
+                $productos->descripcion = $listaDeParametros['descripcion'];
+                $productos->precio = $listaDeParametros['precio'];
+                $productos->imagen = $listaDeParametros['imagen'];
+
+                Producto::CrearProductos($productos);
+                $response->getBody()->write(json_encode($productos));
+
+                return $response;
+        }
+        public function obtenerFormMod($request, $response, $args){
+            $listaDeParametros = $request->getParsedBody();
+            $productos = new Producto();
+            $productos->id_producto = $listaDeParametros['id_producto'];
+
+            $jsonproductos = Producto::FormModProducto($productos);
+
+            $response->getBody()->Write(json_encode($jsonproductos));
+            return $response ->withHeader('Content-Type', 'application/json');
+
+
+        }
+        public function ModProductos($request, $response, $args){
+            $listaDeParametros = $request->getParsedBody();
+
+            $productos = new Producto();
+            $productos->nombre = $listaDeParametros['nombre'];
+            $productos->descripcion = $listaDeParametros['descripcion'];
+            $productos->precio = $listaDeParametros['precio'];
+            $productos->imagen = $listaDeParametros['imagen'];
+            
+            
+
+            Producto::ModificarProductos($productos);
+            $response->getBody()->write("producto modificado");
+
+            return $response;
+
+        }
+        public function DeleteProductos($request, $response, $args){
+            $listaDeParametros = $request->getParsedBody();
+            $productos=  new Producto();
+            $productos->id_producto =  $listaDeParametros['id_producto'];
+
+            Producto::EliminarProductos($productos);
+            $response->getBody()->Write("producto eliminado");
+            return $response;
+
+        }
+
+
+}
+
+?>
