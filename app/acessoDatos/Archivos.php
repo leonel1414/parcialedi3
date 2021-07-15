e9cfb5a1decb053b755cfcb903ffd449213b3373
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2021-07-14 23:17:44 -0300

    a

---
+++ app/acessoDatos/Archivos.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Archivos{
+
+   
+    public static function leerArchivo($nombre){
+        //r
+        $salida ="";
+        $arch = fopen($nombre,'r');
+
+        while (!feof($arch)){
+
+            $salida .= fgets($arch);
+        }
+        
+
+        fclose($arch);
+
+        return $salida ;
+
+    }
+
+}
