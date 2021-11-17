4c8cdd34cb9876ff142c363796e73b22492af963
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2021-11-17 20:16:07 -0300

    pajero

--- app/entidades/productos.php
+++ app/entidades/productos.php
@@ -32,34 +32,34 @@
     return;
 }
 
- public static function EliminarProductos($productos)
+ public static function EliminarProductos($produ)
 {
 
 
     $objAccesoDatos = AccesoDatos::obtenerInstancia();
     $consulta = $objAccesoDatos->prepararConsulta("DELETE FROM `productos` WHERE id_producto = ?");
   
-    $consulta->execute([$productos->id_producto]);
+    $consulta->execute([$produ->id_producto]);
 
    
     return;
 }
-public static function FormModProducto($productos)
+public static function FormModProducto($produ)
     {
    
         $objAccesoDatos = AccesoDatos::obtenerInstancia();
         $consulta = $objAccesoDatos->prepararConsulta("SELECT * FROM `productos` WHERE `id_producto`= ?");
 
-        $consulta->execute([$productos->id_producto]);
+        $consulta->execute([$produ->id_producto]);
 
         return $consulta->fetchAll(PDO::FETCH_CLASS, 'productos');
 }
- public static function ModificarProductos($productos)
+ public static function ModificarProductos($produ)
        {
 
             $objAccesoDatos = AccesoDatos::obtenerInstancia();
             $consulta = $objAccesoDatos->prepararConsulta("UPDATE `productos` SET `titulo` = ? , `descripcion` = ? , `puntaje` = ? , `imagen` = ?, `anio` = ? ,`trailer` = ?  WHERE `id_producto` = ? ");
-            $consulta->execute([$productos->titulo, $productos->descripcion,$productos->puntaje, $productos->imagen, $productos->anio, $productos->trailer, $productos->id_producto]);
+            $consulta->execute([$produ->titulo, $produ->descripcion,$produ->puntaje, $produ->imagen, $produ->anio, $produ->trailer, $produ->id_producto]);
             return;
        }
 }
